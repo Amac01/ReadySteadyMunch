bc45c408eac293d52b1ce5fa1ddc8de4c2f11af3
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2019-01-24 17:11:24 +0000

    wip

--- app/src/main/java/com/example/readysteadymunch/DisplayMessageActivity.java
+++ app/src/main/java/com/example/readysteadymunch/DisplayMessageActivity.java
@@ -1,25 +1,25 @@
-package com.example.readysteadymunch;
-
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.TextView;
-
-import static com.example.readysteadymunch.MainActivity.*;
-
-public class DisplayMessageActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_display_message);
-
-        //get the Intent that started this activity and extract the string
-        Intent intent = getIntent();
-        String message = intent.getStringExtra(EXTRA_MESSAGE);
-
-        // Capture the layout's TextView and set the string as its text
-        TextView outputs = findViewById(R.id.outputs);
-        outputs.setText(message);
-    }
-}
+//package com.example.readysteadymunch;
+//
+//import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+//import android.os.Bundle;
+//import android.widget.TextView;
+//
+//import static com.example.readysteadymunch.MainActivity.*;
+//
+//public class DisplayMessageActivity extends AppCompatActivity {
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_display_message);
+//
+//        //get the Intent that started this activity and extract the string
+//        Intent intent = getIntent();
+//        String message = intent.getStringExtra(EXTRA_MESSAGE);
+//
+//        // Capture the layout's TextView and set the string as its text
+//        TextView outputs = findViewById(R.id.outputs);
+//        outputs.setText(message);
+//    }
+//}
--- app/src/main/java/com/example/readysteadymunch/MainActivity.java
+++ app/src/main/java/com/example/readysteadymunch/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.readysteadymunch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
 
@@ -15,11 +16,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void sendMessage(View view){
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.addIngredients);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+
+      public void sendMessage(View view){
+          EditText editText = (EditText) findViewById(R.id.ingredients);
+
+          String edit = editText.getText().toString();
+          System.out.println(edit);
+
+      }
+//    public void sendMessage(View view){
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.addIngredients);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
 }
