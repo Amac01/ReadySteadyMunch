45ce8e8f4d66afa9c5410758e1215b4b9067d216
Alpha Mackie <alphamackie@Alphas-MacBook-Pro.local>
2019-01-25 11:00:10 +0000

    Fixed errors that were causing the second activity view to crash

--- app/src/main/java/com/example/readysteadymunch/DisplayMessageActivity.java
+++ app/src/main/java/com/example/readysteadymunch/DisplayMessageActivity.java
@@ -1,25 +1,29 @@
-//package com.example.readysteadymunch;
-//
-//import android.content.Intent;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.widget.TextView;
-//
-//import static com.example.readysteadymunch.MainActivity.*;
-//
-//public class DisplayMessageActivity extends AppCompatActivity {
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_display_message);
-//
-//        //get the Intent that started this activity and extract the string
-//        Intent intent = getIntent();
-//        String message = intent.getStringExtra(EXTRA_MESSAGE);
+package com.example.readysteadymunch;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+
+
+        //get the Intent that started this activity and extract the string
+          Intent intent = getIntent();
+          String message = intent.getStringExtra("EXTRA_MESSAGE");
 //
 //        // Capture the layout's TextView and set the string as its text
-//        TextView outputs = findViewById(R.id.outputs);
-//        outputs.setText(message);
-//    }
-//}
+          TextView textView = findViewById(R.id.ingredientsTxt);
+
+        TextView outputsTxt = findViewById(R.id.outputsTxt);
+        outputsTxt.setText(message);
+    }
+}
--- app/src/main/java/com/example/readysteadymunch/MainActivity.java
+++ app/src/main/java/com/example/readysteadymunch/MainActivity.java
@@ -3,32 +3,43 @@ package com.example.readysteadymunch;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
+
 import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String EXTRA_MESSAGE = "com.example.readySteadyMunch.MESSAGE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//
+//        Intent intent = getIntent();
+//
+//        String message = intent.getStringExtra(EXTRA_MESSAGE);
+//        TextView textView = findViewById(R.id.addIngredients);
+//        textView.setText(message);
+
+
+//        final Button DisplayMessageActivityBtn = (Button)findViewById(R.id.DisplayMessageActivityBtn);
+//        DisplayMessageActivityBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent startIntent = new Intent(getApplicationContext(), DisplayMessageActivity.class);
+//            }
+//        });
+
     }
 
 
       public void sendMessage(View view){
-          EditText editText = (EditText) findViewById(R.id.ingredients);
+          EditText editText = findViewById(R.id.ingredientsTxt);
+          String message = editText.getText().toString();
+          Intent intent = new Intent(this, DisplayMessageActivity.class);
+          intent.putExtra("EXTRA_MESSAGE", message);
+          startActivity(intent);
 
-          String edit = editText.getText().toString();
-          System.out.println(edit);
 
       }
-//    public void sendMessage(View view){
-//        Intent intent = new Intent(this, DisplayMessageActivity.class);
-//        EditText editText = (EditText) findViewById(R.id.addIngredients);
-//        String message = editText.getText().toString();
-//        intent.putExtra(EXTRA_MESSAGE, message);
-//        startActivity(intent);
-//    }
+
 }
