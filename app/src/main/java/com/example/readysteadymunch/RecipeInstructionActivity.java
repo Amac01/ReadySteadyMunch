5fa14ca26c9457c050c0ce90aec2eacb3743a9f8
Nimalan Kirubakaran <nimalan.kirubakaran@digital.cabinet-office.gov.uk>
2019-01-30 11:35:50 +0000

    views displaying correctly for recipe instructions - but no multiline

--- app/src/main/java/com/example/readysteadymunch/RecipeInstructionActivity.java
+++ app/src/main/java/com/example/readysteadymunch/RecipeInstructionActivity.java
@@ -76,7 +76,7 @@ public class RecipeInstructionActivity extends AppCompatActivity {
 
     protected void setUpViews() {
         TextView recipe_title = findViewById(R.id.recipe_title);
-        ImageView recipe_picture = findViewById(R.id.recipe_image);
+        ImageView recipe_picture = findViewById(R.id.recipe_picture);
         TextView recipe_ingredients = findViewById(R.id.recipe_ingredients);
         TextView recipe_nutrition = findViewById(R.id.recipe_nutrition);
         TextView recipe_complete_instructions = findViewById(R.id.recipe_instructions);
@@ -84,15 +84,24 @@ public class RecipeInstructionActivity extends AppCompatActivity {
         Picasso.get().load(recipe_instructions.getImage()).resize(340, 200).into(recipe_picture);
         String complete_ingredients = "";
         String complete_instructions = "";
+        String complete_nutrition = "Ready in minutes: " + recipe_instructions.getReadyInMinutes() +
+                " minutes" +
+                " Vegetarian: " + recipe_instructions.isVegetarian() +
+                " Vegan: " + recipe_instructions.isVegan() +
+                " Gluten Free: " + recipe_instructions.isGlutenFree() +
+                " Dairy Free: " + recipe_instructions.isDairyFree() +
+                " Serves: " + recipe_instructions.getServings();
         for (String element: recipe_instructions.getIngredients()){
-            complete_ingredients += element + "/n";
+            complete_ingredients += element + " /n";
         }
         for (String element: recipe_instructions.getInstructions()){
-            complete_instructions += element + "/n";
+            complete_instructions += element + " /n";
         }
+        System.out.println(complete_ingredients);
+        System.out.println(complete_instructions);
         recipe_ingredients.setText(complete_ingredients);
+        recipe_nutrition.setText(complete_nutrition);
         recipe_complete_instructions.setText(complete_instructions);
-        // Need to do recipe_nutrition
     }
 
     protected void jsonToJava(JSONArray json_array) {
@@ -127,6 +136,17 @@ public class RecipeInstructionActivity extends AppCompatActivity {
                 String instruction = individual_instruction.getString("step");
                 recipe_instructions.addInstructions(instruction);
             }
+
+            System.out.println(recipe_instructions.getTitle());
+            System.out.println(recipe_instructions.getImage());
+            System.out.println(recipe_instructions.getReadyInMinutes());
+            System.out.println(recipe_instructions.isVegetarian());
+            System.out.println(recipe_instructions.isVegan());
+            System.out.println(recipe_instructions.isGlutenFree());
+            System.out.println(recipe_instructions.isDairyFree());
+            System.out.println(recipe_instructions.getServings());
+            System.out.println(recipe_instructions.getIngredients().size());
+            System.out.println(recipe_instructions.getInstructions().size());
 
         } catch (JSONException e) {
             e.printStackTrace();
